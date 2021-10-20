cf775e5c0cd11884f187f8490db6656ba383d41b
Rik <91874742+sr-rik@users.noreply.github.com>
2021-10-20 20:59:38 +0900

    Create data.php

---
+++ data.php
@@ -0,0 +1,72 @@
+<?php
+require_once('class1.php');
+
+$question1 = new Question('<p>机の上からペンケースを落としてしまいました。
+<br>ペンケースの中には、
+<br><span class="item">消しゴム</span>、<span class="item">シャープペンシル</span>、<span class="item">定規</span>、<span class="item">ものさし</span>
+<br>が入っていました。</p>
+<p>この4つの中で、<span class="toi">命を落とした</span>のはどれでしょう？</p> 
+</div>');
+$question2 = new Question('<p><span class="item">春</span>、<span class="item">夏</span>、<span class="item">秋</span>、<span class="item">冬</span>
+<br>一年の中で<span class="toi">一番長い</span>のはこの中のどれでしょう？</p>
+</div>');
+$question3 = new Question('<p>たろう君はお寿司やさんに行きました。
+<br>えび：<span class="item">100円</span>、たまご：<span class="item">100円</span>、まぐろ：<span class="item">100円</span>、大トロ：<span class="item">200円</span>でした。
+<br><span class="toi">中トロ</span>はいくらでしょうか？</p>
+</div>');
+$question4 = new Question('<p>これは4つの部屋の話。
+<br>あなたはケビン、マイク、ジーナ、サリーと一緒にある部屋に閉じ込められている。
+<br>あなたの目的はこの建物から脱出すること。
+<br>この部屋には4つ部屋につながるの扉があり、どの部屋を通り抜けても出口にたどり着く。
+<br>
+<br>1つ目の部屋には猛毒に犯されてしまう<span class="item">有毒ガス</span>が充満している。
+<br>2つ目の部屋には1年以上何も食べていない<span class="item">獰猛なライオンが100匹</span>解き放たれている。
+<br>3つ目の部屋には<span class="item">1000℃に熱せられた油</span>が部屋を満たしている。
+<br>4つ目の部屋には<span class="item">50人の狂った殺人鬼</span>たちが誰か入って来ないかと待ち望んでいる。
+<br>
+<br>ケビンは1つ目の部屋、マイクは2つ目の部屋、ジーナは3つ目の部屋、サリーは4つ目の部屋に入ることにした。
+<br>あなたは<span class="toi">誰と同じ部屋</span>に入れば無事に脱出できるだろうか？</p>
+</div>');
+$question5 = new Question('<p><span class="item">目</span> = 4
+<br><span class="item">鼻</span> = 9
+<br><span class="item">口</span> = 3
+<br><span class="item">耳</span> = ?
+<br>「？」には<span class="toi">何の数字</span>が入るでしょうか？</p>
+</div>');
+$question6 = new Question('<p>人間の<span class="item">心臓</span>は1人に1つしかないとされている。
+<br>しかし、不思議なもので、中には<span class="item">2つ</span>も<span class="item">3つ</span>も心臓を持っている人間がいるらしい。
+<br>いったいその人は<span class="toi">何者</span>なのだろうか？</p>
+</div>');
+$question7 = new Question('<p>ヒント：□＜ー○
+<br>
+<br>(キーワード)
+<br><span class="item">るいる</span>(国)
+<br><span class="item">るる"め</span>(鳥)
+<br><span class="item">るぶた</span>(料理)
+<br>
+<br>上のキーワードをもとにヒントが何を表しているかを<span class="toi">漢字2文字</span>で答えよ。</p>
+</div>');
+$question8 = new Question('<p>(<span class="item">弱い</span>)　…→2→0→5→2→0→5→2→0→5→…　(<span class="item">強い</span>)
+<br>
+<br>何の<span class="toi">遊び</span>を表している？</p>
+</div>');
+$question9 = new Question('<p>確実に<span class="item">近づいて</span>いるのに
+<br>絶対に<span class="toi">辿りつけない</span>のは何？
+<br></p>
+</div>');
+$question10 = new Question('<p><span class="item">ライオン</span>、<span class="item">ダチョウ</span>、<span class="item">シャチ</span>、<span class="item">シマウマ</span>
+<br>この4匹でマラソンをした時
+<br><span class="toi">一番最初にゴール</span>したのはどの生き物？</p>
+</div>');
+$question11 = new Question('<p><span class="item">神奈川</span>、<span class="item">大阪</span>、<span class="item">沖縄</span>、<span class="item">東京</span>
+<br>この中で住民がいつも<span class="toi">眠そう</span>なのはどこの地域の人？</p>
+</div>');
+$question12 = new Question('<p><span class="item">野菜</span>にかける<span class="toi">ドレッシングの日</span>はいつでしょう？</p>
+</div>');
+$question13 = new Question('<p>更新中…
+<br>しばらくお待ちください</p>
+</div>');
+
+$questions=array($question1,$question2,$question3,$question4,$question5,$question6,$question7,$question8,$question9,$question10,$question11,$question12,$question13);
+
+?>
